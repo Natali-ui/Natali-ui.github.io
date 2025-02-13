bd7c77db454f794b0a5d65103d2d4c5ba76b2b1b
Natali-ui <nddiukova@yandex.ru>
2025-02-13 19:12:33 +0300

    Файлы сайта-визитки

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = "Дюкова Наталья QA Инженер";
+const typedTextElement = document.getElementById("typed-text");
+let index = 0;
+
+function typeText() {
+    if (index < text.length) {
+        typedTextElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeText, 100);
+    }
+}
+
+typeText();
